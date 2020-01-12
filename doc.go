e4fb2a84ae6a1bf267f56aa5222c2731d5ab214e
Jonathan Hall <flimzy@flimzy.com>
2020-01-12 20:18:06 +0100

    Add initial go file

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package kiviksql provides a standard sql interface to CouchDB/PouchDB.
+//
+// This package is very experimental, and subject to change drastically without
+// notice. Use at your own risk.
+package kiviksql
